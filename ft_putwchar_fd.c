d07a4f3f350e5684504c0799f0ef784110596ad6
Thibaut FONTAINE <tfontain@e3r2p20.42.fr>
2017-02-22 02:36:39 +0100

    ok

--- ft_putwchar_fd.c
+++ ft_putwchar_fd.c
@@ -6,21 +6,56 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 20:15:38 by tfontain          #+#    #+#             */
-/*   Updated: 2017/02/21 16:06:32 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/02/22 02:35:59 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/libft.h"
 
-void				ft_putwchar_fd(wchar_t c, int fd)
+static void			ft_place10(unsigned char *u, size_t n)
 {
-	unsigned char	*s;
+	size_t			ll;
 
-	s = (unsigned char*)&c;
-	write(fd, s, 1);
-	/*while ((((*s >> 7) & 1) == 1 && ((*s >> 6) & 1) == 0))
+	ll = 0;
+	while (ll < n)
 	{
-		write(fd, s, 1);
-		++s;
-	}*/
+		*((wchar_t*)&(u[n])) >>= 2;
+		u[n] |= 0x80;
+		++ll;
+	}
+}
+
+void				ft_putwchar_fd(wchar_t cin, int fd)
+{
+	unsigned char	*u;
+	size_t			len;
+	wchar_t			c = cin; // on doit faire ca sinon l'ordinateur explose
+
+	u = (unsigned char*)&c;
+	len = ft_strlen((char*)u);
+	ft_memrev(u, len);
+	++len;
+	if (len > 1)
+		while (((*u >> 7) & 1) != 1 && (c <<= 1)); // decale a gauche
+	if (len == 2) // le reste c'est pas sur mais y'a de l'idee
+	{
+		c >>= 3;
+		u[0] |= 0xC0;
+		ft_place10(u, 1);
+	}
+	if (len == 3)
+	{
+		c >>= 4;
+		u[0] |= 0xE0;
+		ft_place10(u, 2);
+	}
+	if (len == 4)
+	{
+		c >>= 5;
+		u[0] |= 0xF0;
+		ft_place10(u, 3);
+	}
+	(void)fd;
+	//write(fd, u, len);
+	ft_putbin(u, 4, 4); ft_putstr("\n"); // ligne a virer
 }
