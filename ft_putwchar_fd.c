f04ced1f131d3dd7aac2ce35143bd1cfd7e7bbf9
Thibaut FONTAINE <tfontain@e2r13p2.42.fr>
2017-02-24 18:01:01 +0100

    ok

--- ft_putwchar_fd.c
+++ ft_putwchar_fd.c
@@ -6,56 +6,32 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 20:15:38 by tfontain          #+#    #+#             */
-/*   Updated: 2017/02/22 02:35:59 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/02/24 17:52:27 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/libft.h"
 
-static void			ft_place10(unsigned char *u, size_t n)
+void	ft_putwchar_fd(wchar_t c, int fd)
 {
-	size_t			ll;
-
-	ll = 0;
-	while (ll < n)
+	if (c <= 0x7F)
+		ft_putchar_fd(c, fd);
+	else if (c <= 0x7FF)
 	{
-		*((wchar_t*)&(u[n])) >>= 2;
-		u[n] |= 0x80;
-		++ll;
+		ft_putchar_fd((c >> 6) + 0xC0, fd);
+		ft_putchar_fd((c & 0x3F) + 0x80, fd);
 	}
-}
-
-void				ft_putwchar_fd(wchar_t cin, int fd)
-{
-	unsigned char	*u;
-	size_t			len;
-	wchar_t			c = cin; // on doit faire ca sinon l'ordinateur explose
-
-	u = (unsigned char*)&c;
-	len = ft_strlen((char*)u);
-	ft_memrev(u, len);
-	++len;
-	if (len > 1)
-		while (((*u >> 7) & 1) != 1 && (c <<= 1)); // decale a gauche
-	if (len == 2) // le reste c'est pas sur mais y'a de l'idee
+	else if (c <= 0xFFFF)
 	{
-		c >>= 3;
-		u[0] |= 0xC0;
-		ft_place10(u, 1);
+		ft_putchar_fd((c >> 12) + 0xE0, fd);
+		ft_putchar_fd(((c >> 6) & 0x3F) + 0x80, fd);
+		ft_putchar_fd((c & 0x3F) + 0x80, fd);
 	}
-	if (len == 3)
+	else if (c <= 0x10FFFF)
 	{
-		c >>= 4;
-		u[0] |= 0xE0;
-		ft_place10(u, 2);
+		ft_putchar_fd((c >> 18) + 0xF0, fd);
+		ft_putchar_fd(((c >> 12) & 0x3F) + 0x80, fd);
+		ft_putchar_fd(((c >> 6) & 0x3F) + 0x80, fd);
+		ft_putchar_fd((c & 0x3F) + 0x80, fd);
 	}
-	if (len == 4)
-	{
-		c >>= 5;
-		u[0] |= 0xF0;
-		ft_place10(u, 3);
-	}
-	(void)fd;
-	//write(fd, u, len);
-	ft_putbin(u, 4, 4); ft_putstr("\n"); // ligne a virer
 }
