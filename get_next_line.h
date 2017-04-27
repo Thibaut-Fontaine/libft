da6e91770b2a1dc5eca31b0eb3ce132c9e906a57
Thibaut FONTAINE <tfontain@e2r10p14.42.fr>
2017-04-27 21:01:37 +0200

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 20:36:22 by tfontain          #+#    #+#             */
-/*   Updated: 2017/01/31 04:19:49 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/27 21:01:00 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,10 @@
 # include <sys/uio.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include "libft.h"
+# include "./includes/libft.h"
 
 # define BUFF_SIZE 5
 # define S (((t_endl*)current->content)->s)
-
-typedef struct	s_endl
-{
-	int			fd;
-	char		*s;
-}				t_endl;
 
 int				get_next_line(const int fd, char **line);
 
